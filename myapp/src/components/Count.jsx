d0e0f1ea2f73f464a71f782096b09c69545749fd
jay-ksloves <jay.prakash@ksolves.com>
2023-10-25 17:56:44 +0530

    updated count using prevState

--- myapp/src/components/Count.jsx
+++ myapp/src/components/Count.jsx
@@ -9,12 +9,26 @@ class Count extends Component {
             count: 0
         }
     }
+
+
+    increment() {
+        // this.setState(
+        //     { count: this.state.count + 1 }
+        //     , () => {
+        //         console.log(`count is ${this.state.count}`);
+        //     });
+        //increment count from previous state rather than current state ---for synchronous
+        this.setState((prevState) => ({ count: prevState.count + 1 }));
+
+    }
     render() {
         return (
             <div>
                 <h1 className='text-center text-info'>Click -count-State</h1> <hr />
                 <h2>Current count value: {this.state.count}</h2>
-                <button onClick={() => this.setState({ count: this.state.count + 1 })} className='btn btn-secondary p-2'>Increment the counter by one </button>
+                {/* <button onClick={() => this.setState({ count: this.state.count + 1 })} className='btn btn-secondary p-2'>Increment the counter by one </button> */}
+
+                <button onClick={() => this.increment()} className='btn btn-secondary p-2'>Click to increment count by 1</button>
             </div>);
     }
 }
