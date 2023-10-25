01d2b65ca2d7ad849ded0da8bd1229e386bd02d2
jay-ksloves <jay.prakash@ksolves.com>
2023-10-25 23:25:27 +0530

    updated keeper app

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import Navbar from './navbar';
-import Footer from './footer';
+import Navbar from './Navbar';
+import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
+
+// function createNotes(notes) {
+//   return <Note title={notes.title} content={notes.content} />;
+
+// }
+
+
 function App() {
   return (
 
     <div>
       <Navbar />
-      <Note />
+      {/* {notes.map(createNotes())} */}
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
 
 }
-
 export default App;
--- keeper/src/components/Navbar.jsx
+++ keeper/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
     return (
         <header>
-            <h1>Keeper</h1>
+            <h1 className=''> <i className="fa-regular fa-lightbulb"></i> Keeper</h1>
         </header>
     );
 }
