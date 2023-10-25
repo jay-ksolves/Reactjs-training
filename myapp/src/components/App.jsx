5a8043e0741c822f6e2984c83c5280c65401d007
jay-ksloves <jay.prakash@ksolves.com>
2023-10-25 15:04:32 +0530

    done class component

--- myapp/src/components/App.jsx
+++ myapp/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Title from './Title';
-import {Content} from './Content';
-
+import { Content } from './Content';
+import Comment from './Comment';
 
 function App() {
     return (
@@ -9,6 +9,7 @@ function App() {
             <p> (from app.jsx)</p>
             <Title />
             <Content />
+            <Comment />
         </div>);
 }
 
---
+++ myapp/src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Comment extends Component
+{
+    render()
+    {
+        return (
+            <h1>Hello this line form class components</h1>
+        );
+
+    }
+}
+export default Comment;
