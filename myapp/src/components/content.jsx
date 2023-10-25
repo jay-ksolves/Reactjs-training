0ba6abc349df257fb61f1974d7d6dd05ec0d8ef0
jay-ksloves <jay.prakash@ksolves.com>
2023-10-25 13:36:10 +0530

    done functional component

--- myapp/src/components/content.jsx
+++ myapp/src/components/content.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function content() {
+// function content() {
+//     return (<p>Hi, this is the content ..(content.jsx).</p>);
+// }
+
+const content = () => {
     return (<p>Hi, this is the content ..(content.jsx).</p>);
+
 }
 
 export default content; 
