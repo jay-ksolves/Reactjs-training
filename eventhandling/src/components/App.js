970917db3a0e0b1f2b8705bb93f053b9cc28186e
jay-ksloves <jay.prakash@ksolves.com>
2023-10-26 15:19:31 +0530

    eventhandling and useState

--- eventhandling/src/components/App.js
+++ eventhandling/src/components/App.js
@@ -60,7 +60,7 @@ function App() {
 
 
   return (
-    <div className='container p-4'>
+    <div className='container '>
       <h1>
         {headingText}
       </h1>
--- myapp/src/components/Usestatelearning.jsx
+++ myapp/src/components/Usestatelearning.jsx
@@ -5,12 +5,18 @@ function Usestate() {
     function increment() {
         setCount(count + 1);
     }
+
+    function decrement()
+    {
+        setCount(count-1);
+    }
     return (
         <div className='text-center'>
             <h1 className='txt-center text-info'>increment Count using UseState</h1>
             <hr />
             <h2>Count is :{count}</h2>
-            <button onClick={increment} className='btn btn-info fa '>+</button>
+            <button onClick={increment} className='btn btn-info fa m-2'>+</button>
+            <button onClick={decrement} className='btn btn-danger fa m-2'>-</button>
         </div>
     )
 }
