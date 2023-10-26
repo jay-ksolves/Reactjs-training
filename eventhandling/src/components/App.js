0d63b5fa5befc277e296c7045ced319d8f057a90
jay-ksloves <jay.prakash@ksolves.com>
2023-10-26 15:10:08 +0530

    done mouse event handling USing useState

---
+++ eventhandling/src/components/App.js
@@ -0,0 +1,73 @@
+// import React, { useState } from "react";
+
+// function App() {
+//   const [headingText, setHeadingText] = useState("Hello");
+//   const [isMousedOver, setMouseOver] = useState(false);
+
+//   function handleClick() {
+//     setHeadingText("Submitted");
+//   }
+
+//   function handleMouseOver() {
+//     setMouseOver(true);
+//   }
+
+//   function handleMouseOut() {
+//     setMouseOver(false);
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>{headingText}</h1>
+//       <input type="text" placeholder="What's your name?" />
+//       <button
+//         style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+//         onClick={handleClick}
+//         onMouseOver={handleMouseOver}
+//         onMouseOut={handleMouseOut}
+//       >
+//         Submit
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+function App() {
+
+  const [headingText, setHeading] = useState("hello");
+  const [buttonText, setButton] = useState("Submit");
+    const [isMousedOver, setMouseOver] = useState(false);
+
+
+
+
+  function clicked() {
+    console.log("you clicked");
+    setHeading('submitted');
+    setButton('Already Submitted');
+  }
+
+   function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+
+  return (
+    <div className='container p-4'>
+      <h1>
+        {headingText}
+      </h1>
+      <input className=''></input>
+      <button onClick={clicked} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} style={{ backgroundColor: isMousedOver ? "black" : "white" }}>{buttonText}</button>
+    </div>
+  );
+}
+
+export default App;
